6c2af2fbb52269fa69a65df06cca321a59452714
Tim Carey-Smith <tim@spork.in>
2008-03-14 00:43:50 +1300

    Add the Long Straight slot

---
+++ lib/steer/slots/long_straight.rb
@@ -0,0 +1,18 @@
+module Steer
+  module Slots
+    class LongStraight < Base
+      register_in :bottom, "Long Straight"
+      
+      def matches?
+        values = turn.dice_values.uniq.sort
+        [1..5, 2..6].any? do |r|
+          (values & r.to_a).size >= 5
+        end
+      end
+      
+      def points
+        40
+      end
+    end
+  end
+end
